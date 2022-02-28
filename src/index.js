0896e229229d969c89606327eab0c617cf0a95fd
Jesse <bjesse974@gmail.com>
2022-02-27 20:10:34 -0500

    2022: Running a program re-hash

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import Grid from './Grid';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Grid/>j
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// You can change the App tag and run the Grid.js class
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
